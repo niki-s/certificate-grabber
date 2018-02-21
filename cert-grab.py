e4459086aba9034b2be58f52179881c2ef0c39fe
root <root@localhost.localdomain>
2018-02-20 16:50:11 -0800

    csv still not working but there is info

--- cert-grab.py
+++ cert-grab.py
@@ -7,17 +7,18 @@ import sys
 
 # open the file listed in the command line
 if len(sys.argv) > 1:
-	file = open(sys.argv[1], 'r')
+	f = open(sys.argv[1], 'r')
 else:
 	print "No input file specified: cert-grab.py <input ip file>"
 
 # make a list to store (ip,port) pairs in
 iplist = []
-for line in file:
+for line in f:
 	line = line.rstrip("\n")
 	print line
 	iplist.append(line.split(" "))
 
+f.close()
 # fetch and decode certificates
 for ip in iplist:
 	try:
@@ -28,4 +29,31 @@ for ip in iplist:
 	except:
 		ip.append("no cert or error")
 
-print iplist
+# save certificate info in a csv
+for ip in iplist:
+	print ip[2]
+	if ip[2] != "no cert or error":
+		version = ip[2].version
+		fingerprint = ip[2].fingerprint
+		serial = ip[2].serial_number
+		publicKey = ip[2].public_key()
+		notValidBefore = ip[2].not_valid_before
+		notValidAfter = ip[2].not_valid_after
+		issuer = ip[2].issuer
+		subject = ip[2].subject
+		hashAlg = ip[2].signature_hash_algorithm
+		#sigBytes = ip[2].signature
+		#bytes = ip[2].tbs_certificate_bytes
+		
+		#there has to be a better way to do this...but the certificate isn't iterable...
+		c = str(version)+','+str(fingerprint)+','+str(serial)+','+str(publicKey)+','+\
+			str(notValidBefore)+','+str(notValidAfter)+','+str(issuer)+','+str(subject)+','+\
+			str(hashAlg)
+	else:
+		c = "no cert or error"
+
+	f = open(ip[0]+"_cert.txt", 'w')
+	f.write(c)
+	f.close()
+
+	
